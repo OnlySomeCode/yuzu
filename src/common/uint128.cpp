0b179b04a45923bc7acacf5dc93d35d01071d4fb
bunnei <bunneidev@gmail.com>
2020-12-04 00:21:56 -0800

    core: core_timing_util: Optimize core timing math.
    - Avoids a lot of unnecessary 128-bit math for imperceptible accuracy.

--- src/common/uint128.cpp
+++ src/common/uint128.cpp
@@ -50,22 +50,4 @@ u128 Multiply64Into128(u64 a, u64 b) {
     return result;
 }
 
-std::pair<u64, u64> Divide128On32(u128 dividend, u32 divisor) {
-    u64 remainder = dividend[0] % divisor;
-    u64 accum = dividend[0] / divisor;
-    if (dividend[1] == 0)
-        return {accum, remainder};
-    // We ignore dividend[1] / divisor as that overflows
-    const u64 first_segment = (dividend[1] % divisor) << 32;
-    accum += (first_segment / divisor) << 32;
-    const u64 second_segment = (first_segment % divisor) << 32;
-    accum += (second_segment / divisor);
-    remainder += second_segment % divisor;
-    if (remainder >= divisor) {
-        accum++;
-        remainder -= divisor;
-    }
-    return {accum, remainder};
-}
-
 } // namespace Common
--- src/common/uint128.h
+++ src/common/uint128.h
@@ -15,8 +15,4 @@ namespace Common {
 // This function multiplies 2 u64 values and produces a u128 value;
 [[nodiscard]] u128 Multiply64Into128(u64 a, u64 b);
 
-// This function divides a u128 by a u32 value and produces two u64 values:
-// the result of division and the remainder
-[[nodiscard]] std::pair<u64, u64> Divide128On32(u128 dividend, u32 divisor);
-
 } // namespace Common
